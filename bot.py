2d89441ff1c501beac5f77e8a7540be847d886f9
kyleleggate1 <57440226+kyleleggate1@users.noreply.github.com>
2019-12-03 13:26:59 +0000

    Add files via upload

--- bot.py
+++ bot.py
@@ -7,9 +7,6 @@ import sys
 from datetime import datetime
 import math
 
-reload(sys)
-sys.setdefaultencoding('utf8')
-
 randomMsg = ["It's not the best choice, it's Spacer's Choice!",
            "You've tried the best, now try the rest!",
            "At Spacer's Choice, we cut corners so you don't have to!",
@@ -19,46 +16,46 @@ msgIndex = 0;
 msgMax = 4;
 
 
-server = "127.0.0.1"
+server = "10.0.42.17"
 channel = "#test"
 botnick = "probot"
 
 irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print "\nConnecting to:" + server
+print("\nConnecting to:" + server)
 irc.connect((server, 6667))
 
-irc.send('NICK ' + botnick + '\r\n')
-irc.send('USER ' + botnick + ' 0 * :' + botnick + '\r\n')
+irc.send(('NICK ' + botnick + '\r\n').encode('utf8'))
+irc.send(('USER ' + botnick + ' 0 * :' + botnick + '\r\n').encode('utf8'))
 
-irc.send("JOIN " + channel + "\n")
-irc.send("PRIVMSG " + channel + " :Available Commands: !host, !ping, !time, !day, and !leave\n")
+irc.send(("JOIN " + channel + "\n").encode('utf8'))
+irc.send(("PRIVMSG " + channel + " :Available Commands: !host, !ping, !time, !day, and !leave\n").encode('utf8'))
 
 try:
   while 1:
-    text = irc.recv(2048)
+    text = irc.recv(2048).decode('utf-8')
     if len(text) > 0:
-      print text
+      print(text)
     else:
       continue
 
     if text.find("PING") != -1:
-      irc.send("PONG " + text.split()[1] + "\n")
+      irc.send(("PONG " + text.split()[1] + "\n").encode('utf8'))
 
 #Commands start here, be sure to join the #test channel
     if text.find("PRIVMSG " + channel) != -1:
       if text.find(":!host") != -1:
-        irc.send("PRIVMSG " + channel + " :" + str(platform.platform()) + "\n")
+        irc.send(("PRIVMSG " + channel + " :" + str(platform.platform()) + "\n").encode('utf8'))
        
       if text.find(":!ping") != -1:
-        irc.send("PRIVMSG " + channel + " :pong!\n")
+        irc.send(("PRIVMSG " + channel + " :pong!\n").encode('utf8'))
 
       if text.find(":!leave") != -1:
-        irc.send("PRIVMSG " + channel + " :Time to leave!\n")
-        irc.send("QUIT :\n")
+        irc.send(("PRIVMSG " + channel + " :Time to leave!\n").encode('utf8'))
+        irc.send(("QUIT :\n").encode('utf8'))
         sys.exit()
          
       if text.find("!time") != -1:
-        irc.send("PRIVMSG " + channel + " :Current Time: " + datetime.now().strftime('%H:%M:%S') + "\n")
+        irc.send(("PRIVMSG " + channel + " :Current Time: " + datetime.now().strftime('%H:%M:%S') + "\n").encode('utf8'))
            
       if text.find("!day") != -1:
         dayNumber = datetime.today().weekday()
@@ -78,17 +75,17 @@ try:
         elif dayNumber == 6:
           day = "Sunday"
           
-        irc.send("PRIVMSG " + channel + " :Current Day: " + day + "\n")
+        irc.send(("PRIVMSG " + channel + " :Current Day: " + day + "\n").encode('utf8'))
     elif text.find("PRIVMSG " + botnick) != -1:
       index = text.find("!")
       sender = text[1:index]
-      irc.send("PRIVMSG " + sender + " :" + randomMsg[msgIndex] + "\n")
+      irc.send(("PRIVMSG " + sender + " :" + randomMsg[msgIndex] + "\n").encode('utf8'))
       msgIndex += 1
       if msgIndex > msgMax:
         msgIndex = 0
       
 
 except KeyboardInterrupt:
-  irc.send("QUIT :I have to go for now!\n")
-  print "\n"
+  irc.send(("QUIT :I have to go for now!\n").encode('utf8'))
+  print("\n")
   sys.exit()
